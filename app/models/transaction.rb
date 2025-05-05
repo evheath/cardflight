cef4aa2e280432bafd121ef3cf7eb66a17635661
Elliot Heath <elliot@businessfactory.dev>
2025-05-05 14:52:06 -0500

    versioning at 0.1

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,13 @@
 class Transaction < ApplicationRecord
+  def version
+    "0.1"
+  end
   self.primary_key = "transaction_id"
   before_create do
     self.transaction_id ||= SecureRandom.uuid
   end
 
   def as_json(options = {})
-    super(options).except("created_at", "updated_at")
+    super(options).except("created_at", "updated_at").merge(version: version)
   end
 end
