f08ee3567f6be18aa5f26f72eca65f7ac8570e55
Elliot Heath <elliot@businessfactory.dev>
2025-05-06 10:18:53 -0500

    first model unit test

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "parses merchant and amount correctly from raw message" do
+    raw = "309SMAINFRMR108DISCOVER2070100.95"
+    t = Transaction.new(raw_message: raw)
+    t.valid? # triggers before_validation hooks
+
+    assert_equal "SMAINFRMR", t.merchant
+    assert_equal "DISCOVER", t.network
+    assert_equal 10095, t.amount
+  end
 end
