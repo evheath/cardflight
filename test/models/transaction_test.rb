19b39cc822898ac95609375b38305d143ecfe5b1
Elliot Heath <elliot@businessfactory.dev>
2025-05-06 10:25:03 -0500

    truncation unit test

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class TransactionTest < ActiveSupport::TestCase
-  test "parses merchant and amount correctly from raw message" do
-    raw = "309SMAINFRMR108DISCOVER2070100.95"
-    t = Transaction.new(raw_message: raw)
+  test "parses tags from raw message" do
+    t = Transaction.new(raw_message: "309SMAINFRMR108DISCOVER2070100.95502QS")
     t.valid? # triggers before_validation hooks
-
     assert_equal "SMAINFRMR", t.merchant
     assert_equal "DISCOVER", t.network
     assert_equal 10095, t.amount
+  end
+
+  test "truncates merchants to 10 characters" do
+    t = Transaction.new(raw_message: "103JCB502QS316COSTSAVERGROCERY20564.80")
+    t.valid? # triggers before_validation hooks
+    assert_equal "COSTSAVERG", t.merchant
   end
 end
