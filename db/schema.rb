b00e4d038884fba5ceaffd989b4e4689c84e7fa6
Elliot Heath <elliot@businessfactory.dev>
2025-05-07 08:15:24 -0500

    customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_06_135450) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_07_125815) do
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "transactions", primary_key: "transaction_id", id: :string, force: :cascade do |t|
     t.string "raw_message", null: false
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,44 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# upsert customers
+Time.use_zone("Central Time (US & Canada)") do
+  Customer.upsert(
+    {
+      id: 1,
+      first_name: "Sam",
+      last_name: "Smith",
+      active: 1,
+      created_at: Time.new(2024, 1, 1, 23, 44, 51)
+    },
+    unique_by: :id
+  )
+  Customer.upsert(
+    {
+      id: 2,
+      first_name: "Jane",
+      last_name: "Thomas",
+      active: 0,
+      created_at: Time.new(2024, 3, 10, 11, 21, 43)
+    },
+    unique_by: :id
+  )
+  Customer.upsert(
+    {
+      id: 3,
+      first_name: "Larry",
+      last_name: "Jones",
+      active: 1,
+      created_at: Time.new(2024, 7, 16, 14, 21, 35)
+    },
+    unique_by: :id
+  )
+  Customer.upsert(
+    {
+      id: 4,
+      first_name: "Laura",
+      last_name: "Miller",
+      active: 1,
+      created_at: Time.new(2025, 1, 15, 12, 34, 56)
+    },
+    unique_by: :id
+  )
+end
